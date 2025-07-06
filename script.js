71eb809344e8611f304af1a75ca463cccec2a9b2
Ansari-Mohd-Saad <157679873+MohdSaad7766@users.noreply.github.com>
2025-07-06 13:48:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const squares = document.querySelectorAll('.square');
 
 squares.forEach((square, index) => {
-  square.addEventListener('mouseenter', () => {
+  square.addEventListener('mouseover', () => {
     squares.forEach((otherSquare, i) => {
       if (i !== index) {
         otherSquare.style.backgroundColor = '#6F4E37'; // Coffee
@@ -9,7 +9,7 @@ squares.forEach((square, index) => {
     });
   });
 
-  square.addEventListener('mouseleave', () => {
+  square.addEventListener('mouseout', () => {
     squares.forEach(sq => {
       sq.style.backgroundColor = '#E6E6FA'; // Lavender
     });
